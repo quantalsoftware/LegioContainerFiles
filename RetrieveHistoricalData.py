6586f13867589582448f84820f573365c66d5307
Quantal Software <projects@quantalsoftware.com.au>
2019-03-05 15:03:58 +1000

    Updated Connection details

--- RetrieveHistoricalData.py
+++ RetrieveHistoricalData.py
@@ -127,10 +127,12 @@ if __name__ == '__main__':
     print("Initialising")
     boto3.set_stream_logger('', logging.INFO)
     warnings.filterwarnings("ignore")
-    s3 = boto3.resource('s3')
+    session = boto3.Session(aws_access_key_id='AKIAJ4KLOVMVW3D3NZIQ',aws_secret_access_key='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')
+    s3 = session.resource('s3')
+    #s3 = boto3.resource('s3')
     s3bucketName = 'legio-data'
     bucket = s3.Bucket(name=s3bucketName)
-    s3_StorageLocation = "Legio/data/database_data/"
+    #s3_StorageLocation = "Legio/data/database_data/"
     s3_StorageLocation = "database_data/"
 
     baseCode = ""
