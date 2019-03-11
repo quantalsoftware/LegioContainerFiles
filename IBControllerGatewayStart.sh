b272383816a5e8e4891ccd1eb9f477df75eb931c
Quantal Software <projects@quantalsoftware.com.au>
2019-03-11 12:43:39 +1000

    Version update

--- IBControllerGatewayStart.sh
+++ IBControllerGatewayStart.sh
@@ -12,7 +12,7 @@
 #                                                                             +
 #=============================================================================+
 
-TWS_MAJOR_VRSN=975
+TWS_MAJOR_VRSN=972
 IBC_INI=/root/ibcontroller.paper/IBController.ini
 TRADING_MODE=
 IBC_PATH=/root/ibcontroller.paper
