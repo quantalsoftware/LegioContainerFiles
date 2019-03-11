6c947406d91840d5fb855c51b3d2c99c53bae11b
Quantal Software <projects@quantalsoftware.com.au>
2019-03-11 12:43:06 +1000

    Updated files

--- Retrieve2019Data.py
+++ Retrieve2019Data.py
@@ -39,7 +39,7 @@ def main():
 
         print("Retrieving Hourly Data")
         ibConn.requestHistoricalData(ibConn.contracts[contract],resolution="1 hour", 
-                                    end_datetime='{} 22:00:00'.format((datetime.datetime.today()).strftime("%Y%m%d")),lookback="5 Y")
+                                    end_datetime='{} 22:00:00'.format((datetime.datetime.today()).strftime("%Y%m%d")),lookback="1 M")
         waiting = True
         lastLen = 0
         while waiting:
@@ -53,24 +53,6 @@ def main():
                 pass
         time.sleep(5)
 
-        lookbackDays = (datetime.datetime.strptime(ibConn.historicalData[baseCode+'_CASH'].sort_values('datetime').iloc[0].name,"%Y-%m-%d %H:%M:%S")-datetime.datetime(2014,1,1,0,0,0)).days
-        endDate = datetime.datetime.strptime(ibConn.historicalData[baseCode+'_CASH'].sort_values('datetime').iloc[0].name,"%Y-%m-%d %H:%M:%S").strftime("%Y%m%d")
-
-        ibConn.requestHistoricalData(ibConn.contracts[contract],resolution="1 hour", 
-                                    end_datetime='{} 22:00:00'.format(endDate),lookback=str(lookbackDays)+" D")
-        waiting = True
-        lastLen = 0
-        while waiting:
-            try:
-                if len(ibConn.historicalData[baseCode+'_CASH']) > lastLen:
-                    lastLen = len(ibConn.historicalData[baseCode+'_CASH'])
-                    time.sleep(2)
-                else:
-                    waiting = False
-            except KeyError:
-                pass
-        time.sleep(5)       
-
         print("Saving Hourly Data")
         hourlyData = ibConn.historicalData[baseCode+'_CASH']
         hourlyData = hourlyData.drop(['V','OI','WAP'],1).reset_index().sort_values('datetime')
@@ -79,11 +61,11 @@ def main():
         bucket.upload_file('/root/data/hour/'+filename, s3_StorageLocation+filename)
         ibConn.historicalData = {}    
 
-        years = [2018,2017,2016,2015,2014]        
+        years = [2019]        
 
         for year in years:
             print("Retrieving Minute Data "+str(year))
-            for i in range(12,0,-1):
+            for i in range(3,0,-1):
                 d = datetime.datetime(year,i,calendar.monthrange(year,i)[1])
                 dateStr = d.strftime("%Y%m%d")
                 print("Month: "+str(d.strftime("%Y %m")))
@@ -135,18 +117,18 @@ if __name__ == '__main__':
     #s3_StorageLocation = "Legio/data/database_data/"
     s3_StorageLocation = "database_data/"
 
-    baseCode = ""
-    clientId=""
-    host=""
+    baseCode = "GBPUSD"
+    clientId="10001"
+    host="172.50.0.30"
     port=4002
 
-    print("Retrieving Market Symbol Code")
-    baseFiles = os.listdir("/")
-    baseCode = [x[len(x)-6:] for x in baseFiles if "MarketCode_" in x][0]
+    # print("Retrieving Market Symbol Code")
+    # baseFiles = os.listdir("/")
+    # baseCode = [x[len(x)-6:] for x in baseFiles if "MarketCode_" in x][0]
 
-    baseConfigInfo = pd.read_csv("./BaseConfigInfo.csv")
-    thisContainer = baseConfigInfo.loc[baseConfigInfo['Symbol']==baseCode]
-    clientId = thisContainer['IB Client ID'].item()
-    host = thisContainer['IBGatewayIP'].item()
+    # baseConfigInfo = pd.read_csv("./BaseConfigInfo.csv")
+    # thisContainer = baseConfigInfo.loc[baseConfigInfo['Symbol']==baseCode]
+    # clientId = thisContainer['IB Client ID'].item()
+    # host = thisContainer['IBGatewayIP'].item()
 
     main()
