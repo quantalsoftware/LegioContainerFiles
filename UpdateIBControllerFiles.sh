b81ed948a9199fa726df731f708c9eb3ecf14d8f
Quantal Software <projects@quantalsoftware.com.au>
2019-03-26 16:44:24 +1000

    update script files

--- UpdateIBControllerFiles.sh
+++ UpdateIBControllerFiles.sh
@@ -2,4 +2,3 @@
 cp -a ~/LegioContainerFiles-master/IBControllerGatewayStart_$1.sh ~/ibcontroller.paper/IBControllerGatewayStart.sh
 cp -a ~/LegioContainerFiles-master/IBController_$1.ini ~/ibcontroller.paper/IBController.ini
 cp -a ~/LegioContainerFiles-master/jts_$1.ini ~/Jts/jts.ini
-Done
